2fc9517264af198cd5612a4f880a73e722ab0eda
AdamK2 <ad200@gazeta.pl>
2018-04-11 02:14:41 +0100

    Add comments to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 // scripts.js
 
 var name = prompt('Enter your name');
-alert('Hello, ' + name);
+// alert('Hello, ' + name);
 console.log('Hello, ' + name);
+
+// Comment no. 2
+
+/* A very long 
+block comment no. 3 */
