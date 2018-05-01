dc6aa3661872e9562abb3cb4fa0a48a68dfc40f4
AdamK2 <ad200@gazeta.pl>
2018-05-01 19:33:26 +0100

    Change js

--- js/scripts.js
+++ js/scripts.js
@@ -3,21 +3,11 @@ var add = document.getElementById('addElem');
 
 
 add.addEventListener('click', function(e) {
-  var element = document.createElement('li');
-  element.innerHTML = 'item';
-  var itemsByTagName = document.getElementsByTagName('li'); 
-		  
-	  
-  list.appendChild(element);
-  
-  /*console.log(itemsByTagName.length) */
-  
-  /* var dlugosc = document.getElementsByTagName.length;
-	console.log(dlugosc)  */
-	
+	var itemsByTagName = document.getElementsByTagName('li');
 	var dlugosc = itemsByTagName.length;
-	console.log(dlugosc)
-	
+	var element = document.createElement('li');
+	element.innerHTML = 'item ' + dlugosc;
+	list.appendChild(element); 	
 }); 
 
 
@@ -27,17 +17,3 @@ add.addEventListener('click', function(e) {
 
 
 
-
-
-/* function checkOnclickEvent(param) {
-  console.log(param);
-}
-var inputElem = document.getElementById('js-inputValue'),
-    imageElem = document.getElementById('js-image');
-
-imageElem.addEventListener('click', function(e) {
-  checkOnclickEvent('image was clicked')
-});
-inputElem.addEventListener('keypress', function(e) {
-  e.target.value += ' test ';
-}); */
